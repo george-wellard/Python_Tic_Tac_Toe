06a6781f9df58554b7e87cb4a796e40ecf0b3e8b
wellard067 <georgewellard03@gmail.com>
2024-09-01 17:35:58 +0100

    Fixed player change & win cond

--- Python_Tic_Tac_Toe/Python_Tic_Tac_Toe.py
+++ Python_Tic_Tac_Toe/Python_Tic_Tac_Toe.py
@@ -11,42 +11,66 @@ def print_board(board):
     print(board[4] + "_|_" + board[5] + "_|_" + board[6])
     print(board[7] + " | " + board[8] + " | " + board[9])
 
-
-
 def player_input(player):
 
-     number = input()
-     
-     board[int(number)] = player
+    print("It's " + player + " 's turn")
 
-
-def player_turn(player):
-
-    if player == 'X':
-        player = player.replace('X', 'O')
-    else:
-        player = player.replace('O', 'X')
-
-
-
-def check_win(board):
-
-    return False
+    number = input()
+    board[int(number)] = player
 
 def game():
 
     turns = 0
-
-    player = 'X'
+    current_player = 'X'
 
     while(turns < 9):
        print_board(board)
-       player_input(player)
-       player_turn(player)
-       check_win(board)
+       player_input(current_player)
+
+       if board[1] == board[2] and board[1] == board[3] and board[1] == 'X' or board[1] == board[2] and board[1] == board[3] and board[1] == 'O':
+           print("Game Over")
+           print(current_player + " has won!")
+           break
+       elif board[4] == board[5] and board[4] == board[6] and board[4] == 'X' or board[4] == board[5] and board[4] == board[6] and board[4] == 'O':
+           print("Game Over")
+           print(current_player + " has won!")
+           break
+       elif board[7] == board[8] and board[7] == board[9] and board[7] == 'X' or board[7] == board[8] and board[7] == board[8] and board[7] == 'O':
+           print("Game Over")
+           print(current_player + " has won!")
+           break
+       elif board[1] == board[4] and board[1] == board[7] and board[1] == 'X' or board[1] == board[4] and board[1] == board[7] and board[1] == 'O':
+           print("Game Over")
+           print(current_player + " has won!")
+           break
+       elif board[2] == board[5] and board[2] == board[8] and board[2] == 'X' or board[2] == board[5] and board[2] == board[8] and board[2] == 'O':
+           print("Game Over")
+           print(current_player + " has won!")
+           break
+       elif board[3] == board[6] and board[3] == board[9] and board[3] == 'X' or board[3] == board[6] and board[3] == board[9] and board[3] == 'O':
+           print("Game Over")
+           print(current_player + " has won!")
+           break
+       elif board[1] == board[5] and board[1] == board[9] and board[1] == 'X' or board[1] == board[5] and board[1] == board[9] and board[1] == 'O':
+           print("Game Over")
+           print(current_player + " has won!")
+           break
+       elif board[3] == board[5] and board[3] == board[7] and board[3] == 'X' or board[3] == board[5] and board[3] == board[7] and board[3] == 'O':
+           print("Game Over")
+           print(current_player + " has won!")
+           break
+       
+
+       if current_player == 'X':
+        current_player = 'O'
+       else:
+        current_player = 'X'
+
        turns += 1
 
+
     print_board(board)
+
 
 game()
 
